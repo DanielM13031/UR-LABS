5e64fa9600a704e79ae082f563c3bc828bb7613e
DanielM <54962223+DanielM13031@users.noreply.github.com>
2024-10-16 12:35:23 -0500

    login de credenciales

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import Sequelize from './config/database.js';
 import users from './models/users.js'
+import bcrypt from 'bcryptjs';
 const app = express();
 const port  = 5000;
 app.use(express.json());
@@ -19,15 +20,47 @@ Sequelize.sync({alter:true})
     console.error('error en la sincronizacion:', error)
 });
 
+//peticiones para el login
+
+app.post('/login',async (req,res) => {
+    const {mail, password} = req.body;
+
+    try {
+        const user = await users.findOne({where:{mail} });// buscar usuario
+
+        if(!user){
+            return res.status(401).json({message: 'Usuario no encontrado'})
+        }
+
+        const ismatch =  bcrypt.compare(password.user.pasword); //comparacion de la contrasela con la contraseña encryptada
+
+        if(ismatch){
+            res.json({message: 'inicio ocrrecto'})
+        }else{
+            res.status(401).json({message: 'contrasela invalida'})
+        }
+    }catch(error){
+        res.json({message: 'error en el servidor'})
+    }
+                });
 
 
-//peticiones
-app.get('/api', (req,res) =>{
-    res.json({message: 'holiiiii'});
-});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//init server
 app.listen(port, () =>{
     console.log(`running on ${port}`)
 })
-
