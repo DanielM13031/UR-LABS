0c927d102efc572e663af949142688cb2a51cc28
DanielM <54962223+DanielM13031@users.noreply.github.com>
2024-10-16 22:56:25 -0500

    creacion y verificacion de los tokens jwt

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,20 @@ import cors from 'cors';
 import Sequelize from './config/database.js';
 import users from './models/users.js'
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+
 const app = express();
 const port  = 5000;
+
+
 app.use(express.json());
 app.use(cors());
+
 //Conecciones de la base de datos
 
 Sequelize.sync({alter:true})
-.then(port,()=>{
+.then(()=>{
     console.log(`base sincronizada a los modelos`);
-    app.listen(port, () => {
-        console.log(`Servidor en ${port}`)
-    });
 })
 .catch((error)=> {
     console.error('error en la sincronizacion:', error)
@@ -32,32 +34,45 @@ app.post('/login',async (req,res) => {
             return res.status(401).json({message: 'Usuario no encontrado'})
         }
 
-        const ismatch =  bcrypt.compare(password.user.pasword); //comparacion de la contrasela con la contraseña encryptada
+        const isMatch = await bcrypt.compare(password, user.password); //comparacion de la contrasela con la contraseña encryptada
 
-        if(ismatch){
-            res.json({message: 'inicio ocrrecto'})
+        if(isMatch){
+            const payload = {
+                id : user.mail
+            };
+            const token = jwt.sign(payload, '13031', { expiresIn: '1h' })
+
+            return res.json({message: 'inicio correcto', token})
         }else{
             res.status(401).json({message: 'contrasela invalida'})
         }
     }catch(error){
-        res.json({message: 'error en el servidor'})
+        res.status(500).json({message: 'error en el servidor'})
     }
                 });
 
+//middleware para el token de login
+
+function checktoken (req,res,next){
+    const header = req.headers['authorization'];
+    if (!header){
+        return res.status(403).json({message: 'Token no proporcionado'})
+    }
+    const token = header.split(' ')[1];
+    jwt.verify(token, '13031', (err, decoded) =>{
+        if(err){
+            return res.status(401).json({message: 'Token no valido'})
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/home', checktoken, (req,res)=>{
+    res.json({message: 'Ha ingresado'})
+});
 
 
 //init server
